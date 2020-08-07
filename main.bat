ac58449cb5cf7af9c8252e0c490b784d3acaaf56
patriquefostere <36264161+patriquefostere@users.noreply.github.com>
2020-08-07 17:12:21 +0100

    Add files via upload

--- main.bat
+++ main.bat
@@ -1,15 +1,31 @@
-@echo off
 REM  run getPlaylistsNum.py, then run exportPlaylists.ahk, then run fileNicer.py
-del "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\*.txt" /f /q
-del "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\nice playlist text files\*.txt" /f /q
-REM delete pre-existing playlist text files to make the program simpler; there shouldn't be any important text files in this directory
-start /wait "C:\Users\Surface\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\getPlaylistsNum.py"
-REM wait for getPlaylistsNum.py to finish
-start /wait "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AutoHotkey\AutoHotKey.exe" "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\exportPlaylists.ahk"
-REM wait for exportPlaylists.ahk to finish
-start /wait "C:\Users\Surface\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\fileNicer.py"
-del "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\*.txt" /f /q
-REM Just to keep the folder tidy
+
+@echo off
+if exist "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\*.txt" ^
+    del "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\*.txt" /f /q
+if exist "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\nice playlist text files\*.txt" ^
+    del "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\nice playlist text files\*.txt" /f /q
+REM delete pre-existing playlist text files to make the program simpler; there shouldn't be any important text files in this directory.
+REM i have tried to make the autohotkey file deal with overwriting files but this is easier
+rem should be empty of text files any since we delete them afterwards
+
+start /wait /b "C:\Users\Surface\AppData\Local\Programs\Python\Python38-32\python.exe" ^
+    "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\getPlaylistsNum.py"
+start /wait /b "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AutoHotkey\AutoHotKey.exe" ^
+    "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\exportPlaylists.ahk"
+
+REM delete on-the-go playlists because those occur on my ipod as glitch (or at least they are undesirable)
+if exist "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\On-The-Go.txt" ^ 
+    del "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\On-The-Go.txt"
+
+rem make the playlist.txt files more readable
+start /wait /b "C:\Users\Surface\AppData\Local\Programs\Python\Python38-32\python.exe" ^
+    "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\fileNicer.py" 
+
+if exist "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\*.txt" ^
+    del "C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\*.txt" /f /q
+    REM Just to keep the folder tidy
+
 echo Nicer playlist text files saved to C:\Users\Surface\Desktop\coding\automatingPlaylistFiles\nice playlist text files.
 
 REM problem: if you say you have only 1 playlist, fileNicer seems to not run?
